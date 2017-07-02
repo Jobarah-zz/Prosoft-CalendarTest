827b50fe80237be5a0b68e4ebbf02522091e35bd
Jobarah <barahona.josue@unitec.edu>
2017-07-01 18:04:47 -0600

    [Update] Calendar - started with the component generators

--- src/js/Calendar.js
+++ src/js/Calendar.js
@@ -14,14 +14,40 @@ export default class Calendar extends Component {
 			daysToSpan: props.daysToSpan,
 			endDate: moment(props.initialDate).add(props.daysToSpan, 'day'),
 			countryCode: 'US',
+			month: moment(props.initialDate).format('MM'),
+			year: moment(props.initialDate).format('YYYY'),
+			weeks: ''
 		};
+
+		this.generateEmptyWeek = this.generateEmptyWeek.bind(this);
 	}
+
+	isInRange(date) {
+		const isValid = moment(date).isBetween(moment(this.state.initialDate).subtract(1, 'day') , this.state.endDate);
+
+		if (isValid)
+			return true;
+		return false;
+	}
+
+
+	generateEmptyWeek() {
+		const days = new Array();
+
+		for (let i = 0; i < 7; i++) {
+			days.push(<Day key={ i } dayNumber={ moment().format('DD') } isHoliday = { false } weekDay = { -1 } isValid = { false }/>);
+		}
+
+		return days;
+	}
+
 
 	render() {
 
 		return (
 			<div className = { 'calendar' }>
-				Calendar goes here
+				<div className = { 'header' }>{header}</div>
+				{ this.state.weeks }
 			</div>
 		);
 	}
